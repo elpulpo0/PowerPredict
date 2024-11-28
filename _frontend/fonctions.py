0568c62beb8a47bfecb7dfde4ff8388d3f9efcab
Valentin Grimaud <veachedev@gmail.com>
2024-11-28 10:27:14 +0100

    commit

--- _frontend/fonctions.py
+++ _frontend/fonctions.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import requests
-import streamlit as st 
+import streamlit as st
 
-def fetch_table_names(api_url="https://powerpredict.onrender.com"):
+def fetch_table_names(api_url="https://powerpredict.onrender.com/tables"):
     """
-    Fetch noms de table depuis l'API
-    args:
-        api_url (str): URL de l'API (e.g., "https://powerpredict.onrender.com/tables")
+    Fetch table names from the API.
+
+    Args:
+        api_url (str): URL of the API (e.g., "https://powerpredict.onrender.com/tables")
+
     Returns:
-        list: Liste des noms de table
+        list: List of table names
     """
-
     try: 
         response = requests.get(api_url)
         if response.status_code == 200:
@@ -18,50 +19,70 @@ def fetch_table_names(api_url="https://powerpredict.onrender.com"):
         else:
             st.error("⛔️ Pas de réponse de l'API")
             return []
-    except Exception as e:
-        st.error(f"🚨 Une erreur est survenue : {e}")
+    except requests.exceptions.RequestException as e:
+        st.error(f"🚨 Une erreur est survenue lors de la connexion à l'API : {e}")
         return []
-    
-    def fetch_table_data(api_url, table_name):
-        """ 
-        Fetch data from a table in the API
-        Args:
-            api_url (str): URL of the API (e.g., "https://powerpredict.onrender.com/data")
-            table_name (str): Name of the table
-        Returns:
-            pd.DataFrame: Data from the table
-        """
-        try:
-            response = requests.get(f"{api_url}/{table_name}")
-            if response.status_code == 200:
-                return pd.DataFrame(response.json())
-            else:
-                st.error(f"🚨 la table {table_name} n'existe pas")
-                return pd.DataFrame()
-        except Exception as e:
-            st.error(f"🚨 Une erreur est survenue : {e}")
+
+def fetch_table_data(api_url, table_name, params=None):
+    """
+    Fetch data from a table in the API.
+
+    Args:
+        api_url (str): URL of the API (e.g., "https://powerpredict.onrender.com/data")
+        table_name (str): Name of the table
+
+    Returns:
+        pd.DataFrame: Data from the table
+    """
+    try:
+        response = requests.get(f"{api_url}/{table_name}", params=params)
+        if response.status_code == 200:
+            return pd.DataFrame(response.json())
+        else:
+            st.error(f"🚨 La table '{table_name}' n'existe pas")
             return pd.DataFrame()
-        
+    except requests.exceptions.RequestException as e:
+        st.error(f"🚨 Une erreur est survenue lors de la connexion à l'API : {e}")
+        return pd.DataFrame()
+
+def fetch_table_data_db(connection, table_name, conditions=None):
+    """
+    Fetch data from a table in the local SQLite database.
+
+    Args:
+        connection (sqlite3.Connection): SQLite connection object
+        table_name (str): Name of the table
+        conditions (dict, optional): Conditions to filter rows, provided as key-value pairs
+
+    Returns:
+        pd.DataFrame: Data from the table
+    """
+    query = f"SELECT * FROM {table_name}"
+    if conditions:
+        where_clause = ' AND '.join([f"{key}='{value}'" for key, value in conditions.items()])
+        query += f" WHERE {where_clause}"
+    return pd.read_sql_query(query, connection)
 
 
 def paginate_data(dataframe, page_size):
     """
-    Fetch data from a table in the API
-    args:
-        dataframe (pd.Dataframe): Dataframe to paginate
-        page_size (int): Nombre de lignes par page
+    Paginate a DataFrame for display.
+
+    Args:
+        dataframe (pd.DataFrame): DataFrame to paginate
+        page_size (int): Number of rows per page
+
     Returns:
-        pd.DataFrame: Dataframe de la page
+        pd.DataFrame: Paginated DataFrame for the current page
     """
     total_rows = len(dataframe)
-    total_pages = (total_rows // page_size)  + (1 if total_rows % page_size != 0 else 0)
+    total_pages = (total_rows // page_size) + (1 if total_rows % page_size != 0 else 0)
 
-    #nro de page
-    page_number = st.sidebar.number_input("Page", min_value=1, max_value=total_pages, value=1,step=1)
+    # Page number selection
+    page_number = st.sidebar.number_input("Page", min_value=1, max_value=total_pages, value=1, step=1)
 
     start_row = (page_number - 1) * page_size
     end_row = start_row + page_size
 
-    st.caption(f"Page {page_number} of {total_pages}")
+    st.caption(f"Page {page_number} sur {total_pages}")
     return dataframe.iloc[start_row:end_row]
-
--- _frontend/main.py
+++ _frontend/main.py
@@ -1,34 +1,60 @@
 import streamlit as st
 from fonctions import fetch_table_names, fetch_table_data, paginate_data 
 
-
-#config esthetique de la page
+# Config esthétique de la page
 st.set_page_config(
     page_title="PowerPredict",
     page_icon="📊",
     layout="wide",
 )
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://powerpredict.onrender.com"
 
-#import CSS apparence
+# Import CSS apparence
 st.markdown(
     """
     <link href="./style.css" rel="stylesheet">
     """,
-unsafe_allow_html=True,
+    unsafe_allow_html=True,
 )
-#fin CSS apparence     
 
-
-#titres et descriptions
+# Titres et descriptions
 st.title("📊 PowerPredict")
-st.write("PowerPredict est un outil de visualisation de données de consommation d'energie")
+st.write("PowerPredict est un outil de visualisation de données de consommation d'énergie")
 
-#Fetch toutes les tables dans la base de données
+loading_html = """
+<div style="text-align: center;">
+    <img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" width="150px" alt="Chargement...">
+    <p>Chargement des données, veuillez patienter...</p>
+</div>
+"""
+#option2
+#<iframe src="https://giphy.com/embed/l3nWhI38IWDofyDrW" 
+#width="100%" height="100%" style="position:absolute" 
+#frameBorder="0" class="giphy-embed" allowFullScreen>
+#</iframe>
+##</div>
+#<p>
+#<a href="https://giphy.com/gifs/thinking-l3nWhI38IWDofyDrW">via GIPHY</a>
+#</p>
+#"""
+loading_placeholder = st.markdown(loading_html, unsafe_allow_html=True)
+
+# Fetch data
 table_names = fetch_table_names(f"{API_URL}/tables")
+loading_placeholder.empty()
+
 if table_names:
-    selected_table = st.sidebar.selectbox("Selectionner une table", table_names)
+    selected_table = st.sidebar.selectbox("Sélectionner une table", table_names)
+
+
+
+# Fetch toutes les tables dans la base de données
+table_names = fetch_table_names(f"{API_URL}/tables")
+selected_table = None  # Initialize selected_table
+
+if table_names:
+    selected_table = st.sidebar.selectbox("Sélectionner une table", table_names)
 
 if selected_table:
     table_data = fetch_table_data(f"{API_URL}/data", selected_table)
@@ -38,9 +64,9 @@ if selected_table:
             "Nombre de lignes par page", min_value=5, max_value=100, value=10, step=5
         )
 
-        paginate_data = paginate_data(table_data, page_size)
-        st.dataframe(paginate_data, use_container_width=True)
+        paginated_data = paginate_data(table_data, page_size)
+        st.dataframe(paginated_data, use_container_width=True)
     else:
-        st.warning("🚫 Pas de donnees pour la table selectionnee")
+        st.warning("🚫 Pas de données pour la table sélectionnée")
 else:
-    st.warning("🚫 Veuillez selectionner une table")
+    st.warning("🚫 Veuillez sélectionner une table")
