8077608a32c2db310a7fd9e0902b782d88603a8c
elpulpo0 <chris.elpulpo@gmail.com>
2024-12-03 10:51:32 +0100

    fix bug

--- _frontend/fonctions.py
+++ _frontend/fonctions.py
@@ -3,28 +3,6 @@ import requests
 import streamlit as st
 from loguru import logger
 
-# Récupération des paramètres de filtrage
-nom_region = st.sidebar.text_input("Nom de la région")
-nom_departement = st.sidebar.text_input("Nom du département")
-annee_consommation = st.sidebar.number_input("Année de consommation", min_value=2020, max_value=2025, step=1)
-surface_declaree = st.sidebar.number_input("Surface déclarée (m²)", min_value=0.0, step=1.0)
-nombre_declaration = st.sidebar.number_input("Nombre de déclarations", min_value=0, step=1)
-vecteur_energie = st.sidebar.text_input("Vecteur d'énergie")
-zone_climatique = st.sidebar.text_input("Zone climatique")
-nom_commune = st.sidebar.text_input("Nom de la commune")
-
-# Mettre les paramètres dans un dictionnaire
-filters = {
-    "region": nom_region,
-    "departement": nom_departement,
-    "annee_consommation": annee_consommation,
-    "surface_declaree": surface_declaree,
-    "nombre_declaration": nombre_declaration,
-    "vecteur_energie": vecteur_energie,
-    "zone_climatique": zone_climatique,
-    "commune": nom_commune
-}
-
 # URL de base de l'API
 API_BASE_URL = "https://powerpredict.onrender.com"
 
@@ -38,9 +16,10 @@ def fetch_data(filters):
     Returns:
         dict: Réponse de l'API au format JSON.
     """
+    valid_filters = {key: value for key, value in filters.items() if value}
     try:
         url = f"{API_BASE_URL}/data?"
-        response = requests.get(url, params=filters)
+        response = requests.get(url, params=valid_filters)
         response.raise_for_status()
         logger.info(f"Requête effectuée : {response.url}")
 
--- _frontend/main.py
+++ _frontend/main.py
@@ -31,12 +31,30 @@ loading_html = """
 """
 loading_placeholder = st.markdown(loading_html, unsafe_allow_html=True)
 
-# Saisie des filtres par l'utilisateur
-region = st.text_input("Région", "guadeloupe", help="Entrez la région à filtrer")
-zone_climatique = st.text_input("Zone Climatique", "GUA", help="Entrez la zone climatique à filtrer")
+# Récupération des paramètres de filtrage
+nom_region = st.sidebar.text_input("Nom de la région")
+nom_departement = st.sidebar.text_input("Nom du département")
+annees_possibles = ["2020", "2021", "2022", "Année de référence"]
+annee_consommation = st.sidebar.selectbox("Choisissez une option :", annees_possibles)
+nombre_declaration = st.sidebar.number_input("Nombre de déclarations", min_value=0, step=1)
+vecteur_energie = st.sidebar.text_input("Vecteur d'énergie")
+zone_climatique = st.sidebar.text_input("Zone climatique")
+nom_commune = st.sidebar.text_input("Nom de la commune")
 
-# Gestion des filtres
-filters = {"region": region, "zone_climatique": zone_climatique}
+# Saisie des filtres par l'utilisateur
+# region = st.text_input("Région", "guadeloupe", help="Entrez la région à filtrer")
+# zone_climatique = st.text_input("Zone Climatique", "GUA", help="Entrez la zone climatique à filtrer")
+
+# Mettre les paramètres dans un dictionnaire
+filters = {
+    "region": nom_region,
+    "departement": nom_departement,
+    "annee_consommation": annee_consommation,
+    "nombre_declaration": nombre_declaration,
+    "vecteur_energie": vecteur_energie,
+    "zone_climatique": zone_climatique,
+    "commune": nom_commune
+}
 
 if st.button("📥 Récupérer les données"):
     with st.spinner("Chargement des données..."):
@@ -45,5 +63,5 @@ if st.button("📥 Récupérer les données"):
         if not response:
             st.error("🚨 Impossible de récupérer les données.")
         else:
-            st.success("✅ Données récupérées avec succès !")
+            st.success("✅ Données récupérées avec succès!")
             st.json(response)
